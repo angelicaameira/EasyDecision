ecec62af91b9f4a08c194c95e6f0db3c69af215e
Antonio Germano <816290+antoniogermano@users.noreply.github.com>
2021-05-29 14:34:38 -0300

    Revert "Desenha primeira tela - tela de decisões "

--- EasyDecision/Decisao/Model/Aluno.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Aluno.swift
-//  EasyDecision
-//
-//  Created by Angélica Andrade de Meira on 29/05/21.
-//
-
-import Foundation
--- EasyDecision/Decisao/controler/DecisaoViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DecisaoAPI.swift
-//  EasyDecision
-//
-//  Created by Angélica Andrade de Meira on 29/05/21.
-//
-
-import Foundation
-import UIKit
