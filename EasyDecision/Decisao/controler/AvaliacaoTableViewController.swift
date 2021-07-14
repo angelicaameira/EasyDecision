bc6dbb583f9b41523a59210dfaff0b315a61095c
Angélica Meira <angel-am@hotmail.com>
2021-07-14 17:53:51 -0300

    Desenha telas de avaliação e de resultados

---
+++ EasyDecision/Decisao/controler/AvaliacaoTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AvaliacaoTableViewController.swift
+//  EasyDecision
+//
+//  Created by Angélica Andrade de Meira on 14/07/21.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class AvaliacaoTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+}
