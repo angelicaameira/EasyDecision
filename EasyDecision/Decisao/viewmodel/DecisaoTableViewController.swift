6b1526f8afca5174ee5c229273fc25accdb3ff5b
Angélica Meira <angel-am@hotmail.com>
2021-11-15 19:55:25 -0300

    Altera tela de decisões para usar view code

--- EasyDecision/Decisao/viewmodel/DecisaoTableViewController.swift
+++ EasyDecision/Decisao/viewmodel/DecisaoTableViewController.swift
@@ -14,10 +14,50 @@ class DecisaoTableViewController: UITableViewController {
     var decisaoSelecionada: Decisao?
     var listaDecisoes: [Decisao]?
     
+    // MARK: tela
+    
+    private lazy var addButton: UIBarButtonItem = {
+        let view = UIBarButtonItem(image: .add, style: .plain, target: self, action: #selector(goToAdicionarDecisao(sender:)))
+        return view
+    }()
+    
+    override func loadView() {
+        self.view = {
+            let tableView = UITableView()
+            tableView.backgroundColor = .systemBackground
+            tableView.dataSource = self
+            tableView.delegate = self
+            return tableView
+        }()
+        
+        self.title = "Decisões"
+        self.navigationController?.navigationBar.topItem?.setRightBarButton(addButton, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        recuperaDecisao()
+        self.tableView.reloadData()
+    }
+    
+    @objc func goToAdicionarDecisao(sender: UIBarButtonItem) {
+        self.navigationController?.pushViewController(AdicionaDecisaoViewController(), animated: true)
+    }
+    
+    func goToEditarDecisao(sender: Any) {
+        let destinationController = AdicionaDecisaoViewController()
+        self.prepare(for: UIStoryboardSegue(identifier: "editarDecisao" , source: self, destination: destinationController), sender: self)
+        self.navigationController?
+            .pushViewController(destinationController, animated: true)
+    }
+    
+    func goToMostrarOpcoes(sender: Any) {
+        let destinationController = OpcoesTableViewController()
+        self.prepare(for: UIStoryboardSegue(identifier: "mostrarOpcoes", source: self, destination: destinationController), sender: self)
+        self.navigationController?.pushViewController(destinationController, animated: true)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        recuperaDecisao()
-        tableView.reloadData()
     }
     
     //MARK: metodos
@@ -54,7 +94,7 @@ class DecisaoTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destinationViewController = segue.destination as? OpcoesTableViewController {
-            if segue.identifier == "mostraOpcoes" {
+            if segue.identifier == "mostrarOpcoes" {
                 destinationViewController.decisao = self.decisaoSelecionada
             }
         }
@@ -67,21 +107,24 @@ class DecisaoTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let acoes = [
-            UIContextualAction(style: .destructive, title: "Delete", handler: { [self] (contextualAction, view, _) in 
+            UIContextualAction(style: .destructive, title: "Delete", handler: { [weak self] (contextualAction, view, _) in
+                guard let self = self else { return }
                 guard let decisao = self.listaDecisoes?[indexPath.row] else { return }
                 do {
                     try decisao.apagaNoBanco()
                     self.recuperaDecisao()
                     tableView.deleteRows(at: [indexPath], with: .automatic)
                 } catch {
-                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "tente novamente"), style: .default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
+                    self.alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "tente novamente"), style: .default, handler: nil))
+                    self.present(self.alert, animated: true, completion: nil)
                     print(error.localizedDescription)
                 }
             }),
-            UIContextualAction(style: .normal, title: "Edit", handler: { (contextualAction, view, _) in
+            UIContextualAction(style: .normal, title: "Edit", handler: { [weak self] (contextualAction, view, _) in
+                guard let self = self else { return }
+                
                 self.decisaoSelecionada = self.listaDecisoes?[indexPath.row]
-                self.performSegue(withIdentifier: "editarDecisao", sender: contextualAction)
+                self.goToEditarDecisao(sender: contextualAction)
             })]
         return UISwipeActionsConfiguration(actions: acoes)
     }
@@ -90,6 +133,6 @@ class DecisaoTableViewController: UITableViewController {
         guard let decisaoSelecionada = self.listaDecisoes?[indexPath.row] else { return }
         
         self.decisaoSelecionada = decisaoSelecionada
-        self.performSegue(withIdentifier: "mostraOpcoes", sender: self)
+        self.goToMostrarOpcoes(sender: self)
     }
 }
